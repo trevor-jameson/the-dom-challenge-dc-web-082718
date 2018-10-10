6b41d368a26139da8792ef787f61e8b3b3c1a7ba
Trevor Jameson <github email address>
2018-10-10 11:46:55 -0400

    1-4 working

--- challenge.js
+++ challenge.js
@@ -4,33 +4,62 @@ let minusButton = document.querySelector("#\\-")
 let likeButton = document.querySelector("#\\<3")
 let pauseButton = document.querySelector("#pause")
 const likes = {}
-// let pauseButton = document.querySelector("#pause")
+
+pauseButton.className = ("running")
 
 // 1. As a user, i should see the timer increment every second once the page has loaded
-function countUp() {
+let countUp = () => {
   counter.innerText = Number(counter.innerText) + 1
 }
-setInterval(countUp, 1000)
+let countInterval = setInterval(countUp, 1000)
+
 //
 // // 2. As a user, i can manually increment and decrement the counter as i like
-plusButton.addEventListener('click', () => {
+let clickUp = () => {
   counter.innerText = Number(counter.innerText) + 1
-})
+}
+plusButton.addEventListener('click', clickUp)
 
-minusButton.addEventListener('click', () => {
+let clickDown = () => {
   counter.innerText = Number(counter.innerText) - 1
-})
+}
+minusButton.addEventListener('click', clickDown)
 
 // 3. As a user, i can like an individual number of the counter. I should see the appropriate number of likes associated with that particular number
-likeButton.addEventListener('click', () => {
+let clickLike = () => {
   if (likes[counter.innerText]) {
     likes[counter.innerText] += 1
   } else {
     likes[counter.innerText] = 1
   }
-})
+}
+
+likeButton.addEventListener('click', clickLike)
 
 // 4. As a user I can pause the game, which should disable all buttons except the pause button, which should now show the text 'resume'
-pauseButton.addEventListener('click', ())
+let changePause = () => {
+  if (pauseButton.classList.contains("running")) {
+    // Remove all previously added event listeners, except changePause
+    clearInterval(countInterval)
+    plusButton.removeEventListener('click', clickUp)
+    minusButton.removeEventListener('click', clickDown)
+    likeButton.removeEventListener('click', clickLike)
+
+    // Change class/state to 'paused'
+    pauseButton.className = ("paused")
+    console.log('now paused')
+  } else if (pauseButton.classList.contains("paused")) {
+    // Add all previously removed event listeners, except changePause
+    setInterval(countInterval, 1000)
+    plusButton.addEventListener('click', clickUp)
+    minusButton.addEventListener('click', clickDown)
+    likeButton.addEventListener('click', clickLike)
+
+    // Change class/state to 'running'
+    pauseButton.className = ("running")
+    console.log('now running')
+  }
+}
+pauseButton.addEventListener('click', changePause)
 
 // 5. As a user I can leave comments on my gameplay, such as "Wow, what a fun game this is"
